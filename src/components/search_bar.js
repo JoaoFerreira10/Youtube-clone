9f7969731a3d1565f939584fe5e14de6ad261f41
Joao <joao.ferreira10@hotmail.com>
2018-09-02 16:27:14 +0100

    created search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''}
+    }
+    
+    render() {
+        return (<input 
+        onChange = {event => this.setState({term: event.target.value})}
+        value = {this.state.term}
+        />);
+    }
+
+}
+export default SearchBar;

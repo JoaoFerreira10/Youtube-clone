ef6ed4e3ba0fa6a188c1bb747275dddecb7ab7ec
Joao <joao.ferreira10@hotmail.com>
2018-09-02 16:13:58 +0100

    call youtube API with fake data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+
+const API_KEY = 'AIzaSyDdQSiwlM6Dr-c_h8deP7AOldufgmQjKEg';
+
+
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []}
+
+    YTSearch({key: API_KEY, term: 'the beatles'}, videos => {
+      
+      this.setState({videos});
+
+      console.log(videos);
+      });
+  }
+
+  render() {
+    return (<div>
+      <SearchBar />
+      <VideoList videos = {this.state.videos}/>
+    </div>);
+  }
+
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));

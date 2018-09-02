8c29bdbb9fc736bd6a78b1c6639ea4ce32b7b35b
Joao <joao.ferreira10@hotmail.com>
2018-09-02 17:22:53 +0100

    add styles

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,10 +10,14 @@ class SearchBar extends React.Component {
     }
     
     render() {
-        return (<input 
-        onChange = {event => this.setState({term: event.target.value})}
-        value = {this.state.term}
-        />);
+        return (
+        <div className="search-bar">
+            <input 
+            onChange = {event => this.setState({term: event.target.value})}
+            value = {this.state.term}
+            />
+        </div>
+    );
     }
 
 }
